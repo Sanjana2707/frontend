c3755e31476c17d9b0a70be6484d88a7859bdaee
Sanjana2707 <sanjanagowda2707@gmail.com>
2025-09-16 12:10:49 +0530

    feat: fetched the data from backend and displayed in view qbanks page

--- src/app/components/view-qbanks/view-qbanks.ts
+++ src/app/components/view-qbanks/view-qbanks.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+//import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-view-qbanks',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './view-qbanks.html',
   styleUrl: './view-qbanks.scss'
 })
-export class ViewQbanks {
+export class ViewQbanks implements OnInit {
+  qbanks = signal<any[]>([]);
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any[]>('https://quiz-server-39z8.onrender.com/api/qbanks').subscribe({
+      next: (data) => this.qbanks.set(data),
+      error: (err) => console.error('Error fetching qbanks:', err)
+    });
+  }
 }
